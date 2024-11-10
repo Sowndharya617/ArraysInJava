07c9336bbb39040fff99b3ee69b85e2c4edb6b29
Sowndharya A <144271999+Sowndharya617@users.noreply.github.com>
2024-11-10 15:48:32 +0530

    Create Sum_Arrays.java

---
+++ Sum_Arrays.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class sumofarr{
+    public static void main(String[]args){
+        int arr[]={2,3,5};
+        int sum =0;
+        
+        for(int i : arr){
+            sum += i;
+        }System.out.print(sum);
+    }
+}
